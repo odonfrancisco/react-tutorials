f3aa394091a56c5f88289dbb8b97781027a62eb4
odonfrancisco <francisco.odon11@gmail.com>
2021-08-11 18:27:29 -0400

    restructured folders, and added some styling

---
+++ cooking_with_react/src/components/RecipeList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Recipe from './Recipe'
+
+export default function RecipeList({ recipes }) {
+    return (
+        <div className="recipe-list">
+            <div>
+                {
+                    recipes.map(recipe => (
+                        <Recipe 
+                            key={recipe.id} 
+                            {...recipe}
+                        />       
+                    ))
+                }
+            </div>
+            <div className="recipe-list__add-recipe-btn-container">
+                <button className="btn btn--primary">
+                    Add Recipe
+                </button>
+            </div>
+        </div>
+    )
+}
